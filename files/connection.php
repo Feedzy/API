7dd49627c55758148bf0cfa604e95f139bf227a2
Lamine Sarr <sarrlaminegk@gmail.com>
2018-12-18 13:16:18 +0000

    debut de la creation d'une api rest en php

---
+++ files/connection.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lam L's Feedzy
+ * Date: 18/12/2018
+ * Time: 12:23
+ */
+
+$connexion = new PDO('mysql:host=localhost;dbname=lamines_api;','lamines','KWKXGlAdLa');
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lam L's Feedzy
+ * Date: 18/12/2018
+ * Time: 11:53
+ */
+include 'files/connection.php';
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/preflight.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/utilities.min.css" rel="stylesheet">
+    <title>API</title>
+</head>
+<body>
+
+<script src="assets/js/main.js"></script>
+</body>
+</html>
